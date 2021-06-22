65eb8a58e651e94855062ca997070233b4faea35
lini <liniaugustinek@gmail.com>
2021-06-22 21:15:17 +0530

    added todo list

--- register/urls.py
+++ register/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('reg', views.reg)
-    ]
+   path('reg', views.reg),
+   path('', views.logintodo, name='logintodo'),
+   path('hometodo', views.hometodo, name='hometodo'),
+   path('craccount', views.craccount, name='craccount')
+   ]
  
--- register/views.py
+++ register/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def reg(req):
  return HttpResponse('Welcome To My Page')
+def logintodo(request):
+ return render(request,'logintodo.html')
+def hometodo(request):
+ return render(request,'hometodo.html')
+def craccount(request):
+ return render(request,'craccount.html')
+ 
