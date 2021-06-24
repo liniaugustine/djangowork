656f5add191b6f5467e30ac0f062e17b7a891d9b
lini <liniaugustinek@gmail.com>
2021-06-24 14:11:29 +0530

    changed url name

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('firstapp/',include('firstapp.urls')),
-    path('', include('register.urls'))
+    path('todo', include('register.urls'))
     ]
  
--- register/urls.py
+++ register/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
    path('reg', views.reg),
-   path('', views.logintodo, name='logintodo'),
+   path('login', views.logintodo, name='logintodo'),
    path('hometodo', views.hometodo, name='hometodo'),
    path('craccount', views.craccount, name='craccount')
    ]
